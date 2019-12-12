4ef875951e74a378df91ace8eebad88a62a8893e
syed <syed@zasti.ai>
2019-12-12 17:08:20 +0530

    Created accordian component

---
+++ src/components/Accordian.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="accordian" v-for="(cdata, index) in collapsibleData" :key="index">
+      <div class="panel-heading" @click="showAnswer(index)">{{cdata.question}}</div>
+      <div class="panel-body" :class="{ active: cdata.active }">{{cdata.answer}}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Accordian",
+  props: {
+    collapsibleData: Array,
+    showAll: Boolean
+  },
+  //   isActive: Boolean,
+  //   data() {
+  //     return {
+  //       isActive: false
+  //     };
+  //   },
+  methods: {
+    showAnswer(index) {
+      //   if (index !== null || index !== undefined) {
+      //     for (let i = 0; i < this.collapsibleData.length; i++) {
+      //       if (index === i) {
+      //         this.isActive = index;
+      //       }
+      //     }
+      //   }
+      this.collapsibleData[index].active = !this.collapsibleData[index].active;
+      if (!this.showAll) {
+        for (let i = 0; i < this.collapsibleData.length; i++) {
+          if (index !== i) {
+            this.collapsibleData[i].active = false;
+          }
+        }
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.accordian {
+  position: relative;
+  width: 500px;
+  margin: auto;
+  display: block;
+  background-color: #d2d2d2;
+  padding: 10px;
+  text-align: left;
+}
+.accordian .panel-heading {
+  background-color: #009999;
+  padding: 5px;
+  color: #fff;
+  font-weight: 500;
+  cursor: pointer;
+}
+.accordian .panel-body {
+  background-color: #ffffff;
+  padding: 5px;
+  color: #000;
+  font-weight: 500;
+  display: none;
+}
+
+.active.panel-body {
+  display: block;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <Carousel :images="images" :loop="true" :autoplay="true" :autoplaySpeed="2000" />
+    <Accordian :collapsibleData="accordianData" :showAll="false" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Carousel from "@/components/Carousel.vue";
+import Accordian from "@/components/Accordian.vue";
 
 export default {
   name: "Home",
   components: {
-    Carousel
+    Carousel,
+    Accordian
   },
   data() {
     return {
@@ -28,6 +31,28 @@ export default {
         },
         {
           imgName: "panama.jpg"
+        }
+      ],
+      accordianData: [
+        {
+          question: "Some Question1",
+          answer: "Anwser for the above question1",
+          active: false
+        },
+        {
+          question: "Some Question2",
+          answer: "Anwser for the above question2",
+          active: false
+        },
+        {
+          question: "Some Question3",
+          answer: "Anwser for the above question3",
+          active: false
+        },
+        {
+          question: "Some Question4",
+          answer: "Anwser for the above question4",
+          active: false
         }
       ]
     };
